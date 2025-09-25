1b9ac0d7e5e9333f6ddf43a6468596c2fd262a8a
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-25 21:09:21 +0300

    installing nuget package

--- LearnRedis/Program.cs
+++ LearnRedis/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StackExchange.Redis;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Connect to localhost on port 6379.
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        IDatabase db = redis.GetDatabase();
+
+        SimpleStringSample(db);
+    }
+
+    static void SimpleStringSample(IDatabase db)
+    {
+        // Store and retrieve a simple string.
+        db.StringSet("ProductName", "Smart phone");
+        Console.WriteLine(db.StringGet("ProductName")); // prints: Smart phone
+
+        Console.ReadLine();
+    }
+}
