979cb84bd1bd87a5b0e04e95be72061d1e7b1db9
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-26 14:50:02 +0300

    adding some methods for string in redis

--- LearnRedis/Program.cs
+++ LearnRedis/Program.cs
@@ -1,23 +1,66 @@
 ﻿using StackExchange.Redis;
 using System;
 
-class Program
+
+
+// Connect to localhost on port 6379.
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+IDatabase db = redis.GetDatabase();
+
+SimpleStringSample(db);
+IncrementSample(db);
+IncrementBySample(db);
+GETSETSample(db);
+MSETSample(db);
+
+void SimpleStringSample(IDatabase db)
 {
-    static void Main(string[] args)
+    // Store a simple string.
+    db.StringSet("ProductName", "Smart phone");
+    //Retrieve a simple string
+    Console.WriteLine(db.StringGet("ProductName"));
+
+    Console.ReadLine();
+}
+void IncrementSample(IDatabase db)
+{
+    // Store a simple string.
+    db.StringSet("Count", 10);
+    //Retrieve a simple string
+    Console.WriteLine(db.StringIncrement("Count"));
+
+    Console.ReadLine();
+}
+void IncrementBySample(IDatabase db)
+{
+    // Store a simple string.
+    db.StringSet("Count2", 10);
+    //Retrieve a simple string
+    Console.WriteLine(db.StringIncrement("Count2", 4));
+
+    Console.ReadLine();
+}
+
+void GETSETSample(IDatabase db)
+{
+    Console.WriteLine(db.StringGetSet("name", "ahmed"));
+    Console.WriteLine(db.StringGetSet("name", "ali"));
+    Console.ReadLine();
+}
+
+// MSET  key1 value1  key2 value2  key3 value3
+void MSETSample(IDatabase db)
+{
+    db.StringSet(new KeyValuePair<RedisKey, RedisValue>[]
     {
-        // Connect to localhost on port 6379.
-        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-        IDatabase db = redis.GetDatabase();
+        new("key:1", "value1"),
+        new("key:2", "value2"),
+        new("key:3", "value3")
+    });
 
-        SimpleStringSample(db);
-    }
-
-    static void SimpleStringSample(IDatabase db)
+    var redisValues = db.StringGet(new RedisKey[] { "key:1", "key:2", "key:3" });
+    foreach (var val in redisValues)
     {
-        // Store and retrieve a simple string.
-        db.StringSet("ProductName", "Smart phone");
-        Console.WriteLine(db.StringGet("ProductName")); // prints: Smart phone
-
-        Console.ReadLine();
+        Console.WriteLine(val.ToString());
     }
 }
