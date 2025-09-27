37f2c559a6b1c5145a85c520c801ed01a056be3d
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-27 19:13:09 +0300

    adding publish and subscribe

---
+++ RedisPubSub.Publish/Program.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+using System;
+
+// Connect to Redis on localhost
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+IDatabase db = redis.GetDatabase();
+
+
+PublishSample(db);
+
+void PublishSample(IDatabase db)
+{
+    while (true)
+    {
+        Console.WriteLine("Enter your message here:");
+        string message = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(message))
+        {
+            Console.WriteLine("Empty message, exiting...");
+            break;
+        }
+
+        db.Publish("channel2", message);
+        Console.WriteLine($"Message published: {message}");
+    }
+}
---
+++ RedisPubSub.Subscribe/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+
+// Connect to Redis on localhost
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+IDatabase db = redis.GetDatabase();
+
+SubscribeSample(db);
+
+void SubscribeSample(IDatabase db)
+{
+    var subscriber = redis.GetSubscriber();
+
+    subscriber.Subscribe("channel2", (channel, message) =>
+    {
+        Console.WriteLine($"message recieved = {message}");
+    });
+
+    Console.ReadLine();
+}
